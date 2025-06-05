385d5cab80df9524510a0bab19b80d527e8f9542
Vlado Brlečić <vlado.brlecic@predavaci.algebra.hr>
2025-06-05 18:50:58 +0200

    Dodan Movies.Client projekt.

---
+++ Movies.Client/Controllers/MoviesController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Client.Models;
+
+namespace Movies.Client.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public MoviesController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:7268/");
+        }
+
+        // GET: Movies
+        public async Task<IActionResult> Index()
+        {
+            var movies = await _httpClient.GetFromJsonAsync<List<Movie>>("api/Movies");
+
+            return View(movies);
+        }
+
+        // GET: Movies/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            var movie = await _httpClient.GetFromJsonAsync<Movie>($"api/Movies/{id}");
+
+            return View(movie);
+        }
+
+        // GET: Movies/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movies/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Movie movie)
+        {
+            var response = await _httpClient.PostAsJsonAsync("api/Movies", movie);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(movie);
+        }
+
+        // GET: Movies/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var movie = await _httpClient.GetFromJsonAsync<Movie>($"api/Movies/{id}");
+
+            return View(movie);
+        }
+
+        // POST: Movies/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Movie movie)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"api/Movies/{id}", movie);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(movie);
+        }
+    }
+}
---
+++ Movies.Client/Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace Movies.Client.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Genre { get; set; }
+        public string? ReleaseYear { get; set; }
+    }
+}
