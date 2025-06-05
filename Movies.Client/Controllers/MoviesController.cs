39593a207c04713648e275896c51c9d35a94de2a
Vlado Brlečić <vlado.brlecic@predavaci.algebra.hr>
2025-06-05 19:25:16 +0200

    Export json-a.

--- Movies.Client/Controllers/MoviesController.cs
+++ Movies.Client/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Movies.Client.Models;
+using Newtonsoft.Json;
 
 namespace Movies.Client.Controllers
 {
@@ -17,6 +18,10 @@ namespace Movies.Client.Controllers
         public async Task<IActionResult> Index()
         {
             var movies = await _httpClient.GetFromJsonAsync<List<Movie>>("api/Movies");
+
+            ViewBag.Filter = "";
+            ViewBag.OrderBy = "asc";
+            ViewBag.PerPage = 0;
 
             return View(movies);
         }
@@ -72,5 +77,85 @@ namespace Movies.Client.Controllers
 
             return View(movie);
         }
+
+        // GET: Movies/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var movie = await _httpClient.GetFromJsonAsync<Movie>($"api/Movies/{id}");
+
+            return View(movie);
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"api/Movies/{id}");
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Search(string filter, string orderBy = "asc", int perPage = 0)
+        {
+            var query = $"api/Movies/search?filter={filter}&orderBy={orderBy}&perPage={perPage}";
+            var movies = await _httpClient.GetFromJsonAsync<List<Movie>>(query);
+
+            ViewBag.Filter = filter;
+            ViewBag.OrderBy = orderBy;
+            ViewBag.PerPage = perPage;
+
+            return View(nameof(Index), movies);
+        }
+
+        public async Task<IActionResult> Export()
+        {
+            var response = await _httpClient.GetAsync("api/Movies");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return StatusCode((int)response.StatusCode, "Failed to fetch movies.");
+            }
+
+            var jsonString = await response.Content.ReadAsStringAsync();
+
+            var movies = JsonConvert.DeserializeObject<List<Movie>>(jsonString);
+
+            var formattedJson = JsonConvert.SerializeObject(movies, Formatting.Indented);
+
+            var exportPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "exports");
+            Directory.CreateDirectory(exportPath);
+
+            var fileName = $"movies_export_{DateTime.Now:yyyyMMdd_HHmmss}.json";
+            var fullPath = Path.Combine(exportPath, fileName);
+
+            await System.IO.File.WriteAllTextAsync(fullPath, formattedJson);
+
+            TempData["ExportSuccess"] = $"Exported to /exports/{fileName}";
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> ExportFile()
+        {
+            var response = await _httpClient.GetAsync("api/Movies");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return StatusCode((int)response.StatusCode, "Failed to fetch movies.");
+            }
+
+            var jsonString = await response.Content.ReadAsStringAsync();
+
+            var movies = JsonConvert.DeserializeObject<List<Movie>>(jsonString);
+
+            var formattedJson = JsonConvert.SerializeObject(movies, Formatting.Indented);
+            var fileBytes = System.Text.Encoding.UTF8.GetBytes(formattedJson);
+
+            var fileName = $"movies_export_{DateTime.Now:yyyyMMdd_HHmmss}.json";
+            return File(fileBytes, "application/json", fileName);
+        }
+
+
     }
 }
